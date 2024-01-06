8b2014b7c7deb56982b996599c66d66aa1633bb6
Chakhsu.Lau <zexu.liu@live.cn>
2024-01-06 22:05:06 +0800

    feat: status support

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ export * from './loader'
 export type { ProtoFileOptionsType, InitOptionsType } from './schema/loader'
 export type { ClientsOptionsType, ServerOptionsType } from './schema/loader'
 
+// export status
+export { Status } from './schema/status'
+
 // export client
 export type { ClientOptionsType } from './schema/client'
--- src/server/serverError.ts
+++ src/server/serverError.ts
@@ -1,5 +1,7 @@
+import { Status } from '../schema/status'
+
 export const createServerError = (err: any): any => {
-  err.code = err.code || 13
+  err.code = err.code || Status.INTERNAL
   if (typeof err.stack === 'string') {
     const stack = err.stack.split('\n')
     err.messages += ` [Error Message From Server, stack: ${stack[1].trim()}]`
